995f06bbe44086a051b72882dac19ad838c4c3b1
MyLConstruccion <fajardonicolas968@gmail.com>
2026-01-30 11:57:32 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Esto garantiza que funcione en local, en servidor y abriendo el archivo
   server: {
     port: 5500
   }
